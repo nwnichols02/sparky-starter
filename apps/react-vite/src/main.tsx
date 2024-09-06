98087a070a0c944cc5619f153444ca7ced00ed28
Nate Nichols <nate.nichols@utili-source.com>
2024-09-06 17:38:49 -0600

    landing page

--- apps/react-vite/src/main.tsx
+++ apps/react-vite/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './app/App';
 import { enableMocking } from './testing/mocks';
+import { CssBaseline } from '@mui/material';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('No root element found');
@@ -12,6 +13,7 @@ enableMocking().then(() => {
   createRoot(root).render(
     <React.StrictMode>
       <App />
+      <CssBaseline/>
     </React.StrictMode>,
   );
 });
